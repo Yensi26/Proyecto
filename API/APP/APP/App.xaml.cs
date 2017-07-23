7ce957328101a3194ea948d658c89ddfd34a792b
horacioud <20121346@ce.pucmm.edu.do>
2017-07-23 18:48:25 -0400

    Classes

--- API/APP/APP/App.xaml.cs
+++ API/APP/APP/App.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿using APP.Pages;
 using Xamarin.Forms;
 
 namespace APP
@@ -13,7 +9,7 @@ namespace APP
         {
             InitializeComponent();
 
-            MainPage = new APP.MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
